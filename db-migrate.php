279a133959d64a3c869f119bd42a1f4f6b6a142d
Pierre Rouwens <pierre@baselinetelematics.com>
2017-02-15 16:48:06 -0500

    autoload fix

--- db-migrate.php
+++ db-migrate.php
@@ -1,6 +1,6 @@
 <?php
 spl_autoload_register(function ($class_name) {
-    require 'src/'.$class_name . '.php';
+    require './src/'.str_replace("\\", "/",$class_name) . '.php';
 });
 
 use DbMigrate\Comparator\DbContentComparator;
