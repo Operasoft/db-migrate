0459a2592c468a51933e2f9b6a5faedb05bf7675
Christian Labonté <clabonte@baselinetelematics.com>
2017-06-08 16:05:18 -0400

    Initial index.php file

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
 <?php
+function endsWith( $str, $sub ) {
+   return ( substr( $str, strlen( $str ) - strlen( $sub ) ) === $sub );
+}
+
+function listConfigurations() {
+	$dir    = './etc';
+	$files = scandir($dir);
+
+	foreach ($files as $file) {
+		if (endsWith($file, '.json')) {
+			echo '<li><a href="?config='.$file.'">'.$file.'</a></li>';
+		}
+	}
+}
+
+function runScript($config) {
+	echo "Running script $config";
+}
+?>
+
+<html>
+	<body>
+<?php if (!isset($_GET["config"])): ?>
+	<p>Select a configuration to run:</p>
+	<ul>
+		<?php listConfigurations(); ?>
+	</ul>
+<?php else: ?>
+	<?php runScript($_GET["config"]); ?>
+<?php endif; ?>
+	</body>
+</html>
